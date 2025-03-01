7c42db73c00f11a776b20c6f375d36454a7a6b8a
Yash Tambe <martialgamer56@gmail.com>
2025-03-01 19:42:55 +0530

    done

---
+++ operators.py
@@ -0,0 +1,48 @@
+#Operators
+
+# 1.Arithmatic Operators
+# a = 20
+# b = 7
+# print("The addition is ", a+b)
+# print("The subtraction is ", a-b)
+# print("The multiplication is ", a*b)
+# print("The division is ", a/b)
+# print("The remainder is ", a%b)
+# print("The floor division result is ", a//b)
+# print("The exponentiation result is ", a**b)
+
+# 2.Assignment Operators
+# c = 10
+# c == 10
+# print(c)
+# c += 12
+# print(c)
+# c -= 2
+# print(c)
+# c *= 4
+# print(c)
+# c /= 5
+# print(c)
+# c %= 6
+# print(c)
+# c **= 3
+# print(c)
+# c //= 3
+# print(c)
+
+# 3.Comparison Operators
+
+# d = 70
+# e = 23
+# print(d == e)
+# print(d < e)
+# print(d > e)
+# print(d <= e)
+# print(d >= e)
+# print(d != e)
+
+# 4.Logical Operators
+l1 = 12
+l2 = 14
+print(l1 < l2 & l1 > l2)
+# print(not(a > b))
