f249837c1c0e0ccbbacfa2fd554ed9082a69aff6
Yash Tambe <martialgamer56@gmail.com>
2025-03-17 18:31:59 +0530

    done

---
+++ loginpage.py
@@ -0,0 +1,23 @@
+import tkinter
+root = tkinter.Tk()  
+root.geometry("500x500")
+root.config(bg='skyblue')
+
+label1 = tkinter.Label(root, text="Enter Username", font=('Arial 18')) 
+label1.place(x=160, y=20)
+
+uname = tkinter.StringVar() 
+t1 = tkinter.Entry(root, textvariable=uname, font=("Arial 18"))
+t1.place(x=120, y=80)
+
+label2 = tkinter.Label(root, text="Enter Password", font=('Arial 18')) 
+label2.place(x=160, y=150)
+
+passvar = tkinter.StringVar()
+t2 = tkinter.Entry(root, textvariable=passvar, font=("Arial 18"), show="*")  
+t2.place(x=120, y=200)
+
+button = tkinter.Button(root, font=('Times New Roman', 10), text='Login', width=15, height=2, bg='orangered')
+button.place(x=190, y=300)
+
+root.mainloop()
