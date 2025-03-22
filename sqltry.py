8b71f88493bb7201650d27f6be44a67c60e483d3
Yash Tambe <martialgamer56@gmail.com>
2025-03-22 17:03:48 +0530

    done

--- sqltry.py
+++ sqltry.py
@@ -1,35 +1,31 @@
 import sqlite3
 from prettytable import PrettyTable
+while(True):
+    conn = sqlite3.connect("my_database.db")
+    curr = conn.cursor()
+    curr.execute("""
+    CREATE TABLE IF NOT EXISTS student (
+        stu_id INTEGER PRIMARY KEY AUTOINCREMENT, 
+        stu_name TEXT NOT NULL, 
+        stu_course TEXT NOT NULL
+        )
+    """)
+    conn.commit()
+    stu_name = input("Enter Name:")
+    stu_course = input("Enter Course:")
 
-conn = sqlite3.connect("my_database.db")
-curr = conn.cursor()
-curr.execute("DROP TABLE IF EXISTS student")
-
-curr.execute("""
-CREATE TABLE student (
-    stu_id INTEGER PRIMARY KEY AUTOINCREMENT, 
-    stu_name TEXT NOT NULL, 
-    stu_course TEXT NOT NULL
-)
-""")
-conn.commit()
-
-curr.execute("""INSERT INTO student (stu_name, stu_course) VALUES 
-    ('Ravi', 'Mech'), 
-    ('Yash', 'Civil');
-    """
-)
+    curr.execute("INSERT INTO student(stu_name,stu_course) VALUES (?,?)",(stu_name,stu_course))
 
     
-conn.commit()
+    conn.commit()
 
-curr.execute("SELECT * FROM student")
-rows = curr.fetchall()
-table = PrettyTable(["ID","Name","Course"])
-for row in rows:
-    table.add_row(row)
-print(table)
+    curr.execute("SELECT * FROM student")
+    rows = curr.fetchall()
+    table = PrettyTable(["ID","Name","Course"])
+    for row in rows:
+        table.add_row(row)
+    print(table)
 
-print("Table 'student' has been recreated!")
+    print("Table 'student' has been recreated!")
 
 
